ba1c9827d4febb9d4bd380feb1de8791b45cc494
Mylambigai <mylambigai16@gmail.com>
2026-04-01 10:58:06 +0530

    Commands

---
+++ Selenium_basic/src/main/java/browser_commands/Commands.java
@@ -0,0 +1,27 @@
+package browser_commands;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Commands {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.get("https://www.flipkart.com/");
+		String title=driver.getTitle();
+		System.out.println("Title: "+title);
+		System.out.println("Title length: "+title.length());
+		if(("https://www.flipkart.com/").contains(driver.getTitle()))
+			System.out.println("Same");
+		else
+			System.out.println("Not same");
+		String pageSource=driver.getPageSource();
+		//System.out.println("Page source: "+pageSource);
+		System.out.println("Page source length: "+pageSource.length());
+		driver.close();
+
+
+	}
+
+}
