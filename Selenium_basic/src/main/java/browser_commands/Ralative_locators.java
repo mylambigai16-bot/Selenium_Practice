ac57972a75c06a055292f66d1520c6d29195c097
Mylambigai <mylambigai16@gmail.com>
2026-04-06 09:56:29 +0530

    Web element commands

--- Selenium_basic/src/main/java/browser_commands/Ralative_locators.java
+++ Selenium_basic/src/main/java/browser_commands/Ralative_locators.java
@@ -3,18 +3,20 @@ package browser_commands;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Ralative_locators {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new EdgeDriver();
 		System.out.println("Hello Welcome!");
 		driver.manage().window().maximize();
-		driver.get("hhtp://ww.google.com");
+		//driver.get("hhtp://www.google.com");
 		driver.get("https://www.hyrtutorials.com/p/add-padding-to-containers.html");
-		WebElement element1=driver.findElement(By.xpath("//label[text()='First Name ']/following-sibling::input[1]"));
-		element1.sendKeys("Myl");
+		//WebElement element1=driver.findElement(By.xpath("//label[text()='First Name ']/following-sibling::input[1]"));
+		//element1.sendKeys("Myl");
+		driver.findElement(By.cssSelector("input[maxlength='10']")).sendKeys("Myl");
 		WebElement element2=driver.findElement(By.xpath("//label[text()='Last Name']/following-sibling::input[1]"));
 		element2.sendKeys("G");
 		WebElement element3=driver.findElement(By.xpath("//label[text()='Email']/following-sibling::input[1]"));
@@ -23,7 +25,7 @@ public class Ralative_locators {
 		element4.sendKeys("Password");
 		WebElement element5=driver.findElement(By.xpath("//label[text()='Repeat Password']/following-sibling::input"));
 		element5.sendKeys("Password");
-		driver.close();
+		//driver.close();
 	}
 
 }
