6a66789d3ef861fdd0d0f4963b617d0c4209c241
Mylambigai <mylambigai16@gmail.com>
2026-04-27 23:39:39 +0530

    Listerner

--- DataDrivenTestingDemo/src/test/java/com/test/LoginTest.java
+++ DataDrivenTestingDemo/src/test/java/com/test/LoginTest.java
@@ -13,10 +13,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.utilities.DPExcel;
 
+@Listeners(ListenerDemo.class)
 public class LoginTest {
 
 	private static final ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
